7c13e52b0f79806cac8c43d22adc7354cac9d5bb
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2023-12-10 23:07:28 +0530

    UI changes done

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -7,6 +7,7 @@ const Sorter = () => {
   const [sortingType, setSortingType] = useState('single');
   const [sortedArray, setSortedArray] = useState([]);
   const [timeTaken, setTimeTaken] = useState(0);
+  const [error, setError] = useState(null);
 
   const handleInputChange = (e) => {
     setInputArray(e.target.value);
@@ -33,40 +34,63 @@ const Sorter = () => {
   
       const result = await response.json();
       setSortedArray(result.sorted_arrays);
-      setTimeTaken(result.time_ns); // Assuming you've adjusted the time unit
+      setTimeTaken(result.time_ns); 
+      setError(null); 
     } catch (error) {
       console.error('Error during API call:', error);
-      // Handle the error, e.g., display an error message to the user
+      setError(error.message); 
     }
   };
   
 
   return (
-    <div>
-      <h2>Array Sorter</h2>
-      <div>
-        <label>
+    <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
+      <h2 style={{ textAlign: 'center' }}>Array Sorter</h2>
+      <div style={{ marginBottom: '15px' }}>
+        <label style={{ display: 'block', marginBottom: '5px' }}>
           Enter Arrays (e.g., [[3, 1, 2], [5, 4, 6]]):
-          <textarea value={inputArray} onChange={handleInputChange} />
+          <textarea
+            value={inputArray}
+            onChange={handleInputChange}
+            style={{ width: '100%', padding: '8px', marginTop: '5px', boxSizing: 'border-box', borderRadius: '4px', height: '150px', resize: 'vertical' }}
+          />
         </label>
       </div>
-      <div>
-        <label>
+      <div style={{ marginBottom: '15px' }}>
+        <label style={{ display: 'block', marginBottom: '5px' }}>
           Sorting Type:
-          <select value={sortingType} onChange={handleSortingTypeChange}>
+          <select
+            value={sortingType}
+            onChange={handleSortingTypeChange}
+            style={{ width: '100%', padding: '8px', marginTop: '5px', boxSizing: 'border-box', borderRadius: '4px' }}
+          >
             <option value="single">Single</option>
             <option value="concurrent">Concurrent</option>
           </select>
         </label>
       </div>
-      <div>
-        <button onClick={handleSortClick}>Sort</button>
+      <div style={{ textAlign: 'center' }}>
+        <button
+          onClick={handleSortClick}
+          style={{ backgroundColor: '#4caf50', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer', fontSize: '16px' }}
+        >
+          Sort
+        </button>
       </div>
+      {error && (
+        <div style={{ marginTop: '20px', textAlign: 'center', color: 'red' }}>
+          <p>{error}</p>
+        </div>
+      )}
       {sortedArray.length > 0 && (
-        <div>
-          <h3>Sorted Array:</h3>
-          <pre>{JSON.stringify(sortedArray, null, 2)}</pre>
-          <p>Time taken: {timeTaken} ns</p>
+        
+        <div style={{ marginTop: '20px' }}>
+          <h3 style={{ textAlign: 'center' }}>Sorted Array:</h3>
+          <pre style={{ padding: '10px', borderRadius: '4px', backgroundColor: '#f4f4f4', overflowX: 'auto' }}>
+          <p style={{ textAlign: 'center' }}>Time taken: {timeTaken} ns</p>
+
+            {JSON.stringify(sortedArray, null, 2)}
+          </pre>
         </div>
       )}
     </div>
